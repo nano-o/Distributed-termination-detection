e221d74586235428329254aaa51a71962ad12080
Giuliano Losa <giuliano@losa.fr>
2022-08-22 09:27:39 -0700

    add apalache option to speed up invariance checks

---
+++ check-proof.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in Inv1 Inv2 Inv3 Inv4 Correctness; do
+        $APALACHE_HOME/script/run-docker.sh check --init=${i}_ --inv=${i} --length=1 --tuning-options='search.invariantFilter=1->.*' Termination.tla || exit 1
+done
--- check-proofs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in Inv1 Inv2 Inv3 Inv4 Correcntness; do
-        $APALACHE_HOME/script/run-docker.sh check --init=${i}_ --inv=${i} --length=1 Termination.tla || exit 1
-done
