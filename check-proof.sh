fcd86c9e191975fac908192aa08ea87b77c8cbdf
Giuliano Losa <giuliano@losa.fr>
2022-11-06 10:07:25 -0800

    Check that invariants hold initially

--- check-proof.sh
+++ check-proof.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# First we check that those invariants are inductive:
+# First we check that the invariants hold initially:
+for i in Inv1 Inv2 Inv3 Inv4; do
+        $APALACHE_HOME/script/run-docker.sh check --init=Init --inv=${i} --length=0 --no-deadlock Termination.tla || exit 1
+done
+# Then we check that those invariants are inductive:
 for i in Inv1 Inv2 Inv3 Inv4; do
         $APALACHE_HOME/script/run-docker.sh check --init=${i}_ --inv=${i} --length=1 --no-deadlock --discard-disabled=false --tuning-options='search.invariantFilter=1->.*' Termination.tla || exit 1
 done
